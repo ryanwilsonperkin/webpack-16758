fb97456b4c5bcf2999dfe8aaa464afa15f759de3
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2023-02-24 19:16:00 -0500

    Use realContentHash, split by chunks and use contenthash

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,13 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
+    chunkFilename: `[name]-[contenthash].js`,
     path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    realContentHash: true,
+    splitChunks: {
+      chunks: 'all',
+    },
   },
 };
